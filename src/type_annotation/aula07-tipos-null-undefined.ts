2d84e3505c4e5fa49efb9d0fd6138747b700e809
LuisP30 <pimenta.lh2@gmail.com>
2026-09-08 18:11:56 -0300

    tipo null e undefined - alteração nas configurações do TS

---
+++ src/type_annotation/aula07-tipos-null-undefined.ts
@@ -0,0 +1,30 @@
+let x;
+
+if(typeof(x) === 'undefined') x = 20
+
+console.log(x*2)
+
+export function createPerson(firstName: string, lastName?: string): { // type annotation
+  firstName: string;
+  lastName?: string;
+}{ // corpo da função
+  return {
+    firstName,
+    lastName
+  };
+}
+
+
+export function squareOf(x: any){
+  if(typeof(x) === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwoNumber = squareOf(2)
+const squareOfTwoString = squareOf('2')
+
+if(typeof(squareOfTwoNumber) == null){
+  console.log('Conta inválida')
+}else{
+  console.log(squareOfTwoNumber)
+}
